0e016be7f91585d2d50b8d77f14435b397a2c5af
DivCodez <divyambanga@gmail.com>
2026-04-26 03:30:34 -0400

    Created the react app with all dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>GeoAgent</h1>
+      <p>Location intelligence, powered by AI.</p>
+    </div>
+  )
+}
+
+export default App
